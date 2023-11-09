4bd7cf9ddca28852637297b344d766fa415d6867
TheUltimateCoder <no@e.mail>
2023-11-09 22:49:12 +0100

    Add project files.

---
+++ ConsoleApp1/MyTestClass.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Entities;
+
+namespace ConsoleApp1
+{
+    public class MyTestClass : Entity, ICreatedOn, IModifiedOn
+    {
+        public string Name { get; set; }
+
+        public string This { get; set; }
+
+        public string That { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime ModifiedOn { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Driver;
+using MongoDB.Entities;
+
+using System.Linq.Expressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await DB.InitAsync("TestDB",
+                MongoClientSettings.FromConnectionString("mongodb://root:example@127.0.0.1:27017?authSource=admin")
+            );
+
+
+            var entities = new List<MyTestClass>();
+            for (int i = 0; i < 100; i++)
+                entities.Add(new MyTestClass()
+                {
+                    Name = $"Test {i}",
+                    That = $"That {i}",
+                    This = $"This is {i}"
+                });
+
+            await DB.InsertAsync(entities);
+
+            var resultset = await GetAllAsync(x => new { id = x.ID, text = $"{x.Name} ({x.This})" });
+            foreach (var result in resultset)
+                Console.WriteLine($"This is result: {result.id} with {result.text}");
+        }
+
+        public static async Task<IList<TProjection>> GetAllAsync<TProjection>(Expression<Func<MyTestClass, TProjection?>> projectionExpression)
+        {
+            return await DB.Find<MyTestClass, TProjection>()
+                .Project(projectionExpression)
+                .ExecuteAsync();
+        }
+    }
+}
